1e37275bf24b8ccc46e32c82c7f46375b9cb345d
jobanpreet singh <jobanpreetsinghit@gmail.com>
2026-07-31 12:45:57 +0530

    add in footer

--- footer.php
+++ footer.php
@@ -43,7 +43,7 @@
         </div>
 
         <div class="footer-bottom">
-            <p>&copy; 2026 S&N GuardianGrid Technologies. All rights .</p>
+            <p>&copy; 2026 S&N GuardianGrid Technologies. All rights reservedgit .</p>
 
             <div class="footer-links">
                 <a href="privacy.php">Privacy Policy</a>
